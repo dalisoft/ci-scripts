7dfcfa2225142c67a8e3c13cbb29cef22c9e2f7b
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-04-20 21:49:55 +0500

    fix: some repos with single semrel, uses `vv` so fix it

--- fetch-and-release-update.sh
+++ fetch-and-release-update.sh
@@ -44,7 +44,7 @@ if [ -z "${GPG_NO_SIGN-}" ] && [ -n "${GPG_PASSPHRASE-}" ]; then
   echo "Git GPG passphrase set"
 fi
 
-TAG=$(curl -s "https://api.github.com/repos/${repository}/releases/latest" | grep 'tag_name' | cut -d ':' -f2 | cut -d '_' -f2 | cut -d '/' -f2 | rev | cut -c3- | rev | tr '"' 'v' | xargs)
+TAG=$(curl -s "https://api.github.com/repos/${repository}/releases/latest" | grep 'tag_name' | cut -d ':' -f2 | cut -d '_' -f2 | cut -d '/' -f2 | rev | cut -c3- | rev | tr '"' 'v' | sed "s/vv/v/g" | xargs)
 echo "Git tag was acqiured"
 
 if ! git rev-parse "refs/tags/${TAG}" >/dev/null 2>&1; then
